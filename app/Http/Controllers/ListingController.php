0c3bb0e3f86a0956a22dd0af8dde431b11c11aa8
Darren Yates <d.yates@live.de>
2023-05-10 14:52:11 +0200

    create listing functionality implemented

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -4,13 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Models\Listing;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class ListingController extends Controller
 {
     // get and show all listings
     public function index() {
         return view('listings.index', [
-            'listings' => Listing::latest()->filter(request(['tag', 'search']))->get()
+            'listings' => Listing::latest()->filter(request(['tag', 'search']))->paginate(6)
         ]);
     }
 
@@ -19,5 +20,27 @@ class ListingController extends Controller
         return view('listings.show', [
             'listing' => $listing
         ]);
+    }
+
+    // show create form
+    public function create() {
+        return view('listings.create');
+    }
+
+    // store listing data
+    public function store(Request $request) {
+        $formFields = $request->validate([
+            'title' => 'required',
+            'company' => ['required', Rule::unique('listings', 'company')],
+            'location' => 'required',
+            'website' => 'required',
+            'email' => ['required', 'email'],
+            'tags' => 'required',
+            'description' => 'required'
+        ]);
+
+        Listing::create($formFields);
+
+        return redirect('/')->with('message', 'Listing created successfully');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,14 @@ use App\Models\Listing;
 // all listings
 Route::get('/', [ListingController::class, 'index']);
 
+// show create form
+Route::get('/listings/create', [ListingController::class, 'create']);
+
+// store listing data
+Route::post('/listings', [ListingController::class, 'store']);
+
+
+
+
 // single listing
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
-
-
